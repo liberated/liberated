b88760509b16d46f46ffc0d44d29fdf31faaf530
derrell <derrell@09f7d036-9b2a-0410-8fbe-9cff4eb17569>
2011-06-27 14:49:40 +0000

    Refactor much of the common code in each backend's Rpc.js file into a common superclass for them. This also allows overriding the (now) static service factory, for custom processing.
    git-svn-id: https://qooxdoo-contrib.svn.sourceforge.net/svnroot/qooxdoo-contrib@21237 09f7d036-9b2a-0410-8fbe-9cff4eb17569

--- source/class/rpcjs/sim/Rpc.js
+++ source/class/rpcjs/sim/Rpc.js
@@ -21,19 +21,16 @@
  */
 qx.Class.define("rpcjs.sim.Rpc",
 {
-  extend : qx.core.Object,
+  extend : rpcjs.AbstractRpcHandler,
 
   construct : function(services, url)
   {
-    // Save the services map.
-    this.__services = services;
-    
+    // Call the superclass constructor
+    this.base(arguments, services);
+
     // Save the URL
     this.setUrl(url);
 
-    // Get an RPC Server instance.
-    this.__rpcServer = new rpcjs.rpc.Server(
-      qx.lang.Function.bind(this.__serviceFactory, this));
 
     // Register ourself as a handler for the specified URL.
     rpcjs.sim.Simulator.registerHandler(
@@ -52,73 +49,6 @@ qx.Class.define("rpcjs.sim.Rpc",
 
   members :
   {
-    /** Our instance of the JSON-RPC server */
-    __rpcServer : null,
-    
-    /** The map of services we've been provided */
-    __services  : null,
-    
-    /**
-     * The service factory takes a method name and attempts to produce a
-     * service method that corresponds to that name. In this implementation,
-     * it concatenates the method name to the name of the variable holding the
-     * service map, and looks for corresponding method.
-     * 
-     * @param fqMethodName {String}
-     *   The fully-qualified name of the method to be called.
-     * 
-     * @param protocol {String}
-     *   The JSON-RPC protocol being used ("qx1", "1.0", "2.0")
-     * 
-     * @param error {rpcjs.rpc.error.Error}
-     *   An error object to be set if an error is encountered in instantiating
-     *   the requested serviced method.
-     * 
-     * @return {Function}
-     *   The service method associated with the specified method name.
-     */
-    __serviceFactory : function(fqMethodName, protocol, error)
-    {
-      var             method;
-
-      // Append the fully-qualified method name to the services map and
-      // evaluate it in hopes of getting a method reference.
-      try
-      {
-        // We have a dot-separated fully-qualified method name. We want to
-        // access that entry in the map of maps in this.__services. Using the
-        // index notation won't work, since it's multiple levels deep
-        // (i.e. fqMethodName might be something like "a.b.c").
-        // 
-        // FIXME: sterilize fqMethodName before eval().
-        method = eval("this.__services" + "." + fqMethodName);
-        
-        // We might have just gotten null, which also means no such method
-        if (! method)
-        {
-          throw new Error("No such method");
-        }
-      }
-      catch(e)
-      {
-        // No such method.
-        if (protocol == "qx1")
-        {
-          error.setCode(qx.io.remote.RpcError.qx1.error.server.MethodNotFound);
-        }
-        else
-        {
-          error.setCode(qx.io.remote.RpcError.v2.error.MethodNotFound);
-        }
-        error.setMessage(e.message);
-        return null;
-      }
-      
-      // Give 'em the reference to the method they can call.
-      return method;
-    },
-
-    
     /**
      * Process an incoming request which is presumably a JSON-RPC request.
      * 
@@ -166,7 +96,7 @@ qx.Class.define("rpcjs.sim.Rpc",
       responseHeaders.statusText = "";
 
       // Call the RPC server to process this request
-      result = this.__rpcServer.processRequest(jsonData);
+      result = this._rpcServer.processRequest(jsonData);
       return result;
     }
   }
